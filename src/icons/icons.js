55c28ceeec7da32ad260b392d3f0a6cb3e8d2a19
Evgenii Vall <ewgenii.wall@gmail.com>
2024-07-12 01:50:22 +0200

    Import icons

---
+++ src/icons/icons.js
@@ -0,0 +1,17 @@
+import MenuIcon from './menu-fill.svg?react';
+import SearchIcon from './search-line.svg?react';
+import NotesIcon from './todo-fill.svg?react';
+import TrashIcon from './delete-bin-fill.svg?react';
+import StarFilledIcon from './star-fill.svg?react';
+import AddNoteIcon from './sticky-note-add-fill.svg?react';
+import UserIcon from './user-3-fill.svg?react';
+
+export {
+  MenuIcon,
+  SearchIcon,
+  NotesIcon,
+  TrashIcon,
+  StarFilledIcon,
+  AddNoteIcon,
+  UserIcon,
+};
