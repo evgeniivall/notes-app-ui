58bc759eebddffccf1dd763ef8397948d78a400b
Evgenii Vall <ewgenii.wall@gmail.com>
2024-08-12 18:15:13 +0200

    [UI] Center button label when there is no icon

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,7 +3,15 @@ import styles from './Button.module.css';
 function Button({ onClick, icon, label, type }) {
   return (
     <button className={`${styles.button} ${styles[type]}`} onClick={onClick}>
-      {icon} {label && <span className={styles.label}>{label}</span>}
+      {icon}
+      {label && (
+        <span
+          className={styles.label}
+          style={!icon ? { textAlign: 'center' } : {}}
+        >
+          {label}
+        </span>
+      )}
     </button>
   );
 }
