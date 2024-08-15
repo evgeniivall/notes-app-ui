7a3527ed22f823b69b85218ae5cef0dd4055853e
Evgenii Vall <ewgenii.wall@gmail.com>
2024-08-15 16:45:17 +0200

    Implement BreadCrumbs component

---
+++ src/features/notes/BreadCrumbs.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ArrowIcon } from '../../icons/icons';
+import { isMobileDevice } from '../../utils/helpers';
+import Button from '../../ui/Button';
+import styles from './BreadCrumbs.module.css';
+
+const BreadCrumbs = ({ folder, noteTitle }) => {
+  const navigate = useNavigate();
+
+  const renderBackButton = () => (
+    <Button
+      icon={<ArrowIcon style={{ transform: 'rotate(180deg)' }} />}
+      type="secondary"
+      onClick={() => navigate(-1)}
+    />
+  );
+
+  const renderBreadcrumbs = () => {
+    const crumbs = [
+      { label: 'Notes', link: '/notes' },
+      ...(folder
+        ? [{ label: folder.name, link: `/notes?folders=${folder.id}` }]
+        : []),
+      { label: noteTitle },
+    ];
+
+    return crumbs.map((crumb, index) => (
+      <React.Fragment key={index}>
+        {crumb.link ? (
+          <a href={crumb.link} className={styles.linkCrumb}>
+            {crumb.label}
+          </a>
+        ) : (
+          <span className={styles.crumb}>{crumb.label}</span>
+        )}
+        {index < crumbs.length - 1 && (
+          <div className={styles.iconContainer}>
+            <ArrowIcon className={styles.icon} />
+          </div>
+        )}
+      </React.Fragment>
+    ));
+  };
+
+  return (
+    <nav className={styles.breadcrumbs}>
+      {renderBackButton()}
+      {isMobileDevice() ? (
+        <span className={styles.crumb}>{noteTitle}</span>
+      ) : (
+        renderBreadcrumbs()
+      )}
+    </nav>
+  );
+};
+
+export default BreadCrumbs;
