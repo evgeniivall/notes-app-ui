c0e8c7bf33d76efe4950de8d870d0bc2ca2c766c
Evgenii Vall <ewgenii.wall@gmail.com>
2024-08-21 18:45:39 +0200

    Introduce local storage versioning

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9,13 +9,45 @@ export function formatDateToDayMonth(date) {
   return new Date(date).toLocaleDateString('en-US', options);
 }
 
-export function loadFromLocalStorage(name, fallback = []) {
-  const data = localStorage.getItem(name);
-  return data ? JSON.parse(data) : fallback;
+export function loadFromLocalStorage(
+  name,
+  fallback = [],
+  expectedVersion = '1.0',
+) {
+  const storedVersion = localStorage.getItem('version');
+
+  // If version doesn't match or is missing, return fallback and clear local storage
+  if (!storedVersion || storedVersion !== expectedVersion) {
+    console.warn(
+      `Outdated or missing version detected. Clearing localStorage...`,
+    );
+    localStorage.removeItem(name);
+    localStorage.removeItem('version');
+    return fallback;
+  }
+
+  const storedData = localStorage.getItem(name);
+
+  if (!storedData) {
+    return fallback;
+  }
+
+  try {
+    return JSON.parse(storedData);
+  } catch (error) {
+    console.error(
+      `Error parsing localStorage data for key: ${name}. Clearing localStorage...`,
+      error,
+    );
+    localStorage.removeItem(name);
+    localStorage.removeItem('version');
+    return fallback;
+  }
 }
 
-export function saveDataToLocalStorage(name, data) {
+export function saveDataToLocalStorage(name, data, version = '1.0') {
   localStorage.setItem(name, JSON.stringify(data));
+  localStorage.setItem('version', version);
 }
 
 const SM_BREAKPOINT = 768;
